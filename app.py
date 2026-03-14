ce3cb8e8275bff106c508587ddf881d158670e38
Rupali Shewale <rupalishewale2005@gmail.com>
2026-03-14 18:46:03 +0530

    port

--- app.py
+++ app.py
@@ -36,5 +36,10 @@ def dashboard_page():
 def ai_tutor_page():
     return render_template("ai_tutor.html")
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
